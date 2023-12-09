b9b8c52b11336e17399c817a9676edf7e0574d47
ali.rasoulinejad <ali.rasoulinejad75@gmail.com>
2023-12-09 20:24:31 +0330

    add tests

---
+++ top_interview_150/best_time_to_buy_and_sell_stock_test.go
@@ -0,0 +1,28 @@
+package top_interview_150
+
+import (
+	"testing"
+)
+
+func TestBestTimeToBuyAndSellStock(t *testing.T) {
+	testCases := []struct {
+		inputPrices    []int
+		expectedProfit int
+	}{
+		{
+			inputPrices:    []int{7, 1, 5, 3, 6, 4},
+			expectedProfit: 5,
+		},
+		{
+			inputPrices:    []int{7, 6, 4, 3, 1},
+			expectedProfit: 0,
+		},
+	}
+
+	for _, testCase := range testCases {
+		response := bestTimeToBuyAndSellStock(testCase.inputPrices)
+		if response != testCase.expectedProfit {
+			t.Errorf("expected response is: %d, but got %d", testCase.expectedProfit, response)
+		}
+	}
+}
